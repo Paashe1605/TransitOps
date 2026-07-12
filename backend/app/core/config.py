3d4ad586f0c5b614c682f1ae128d15462484a60b
Paashe1605 <168164933+Paashe1605@users.noreply.github.com>
2026-07-12 11:06:50 +0530

    feat: generated initial database migrations

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     
     # Postgres Database Config
-    POSTGRES_SERVER: str = "localhost"
+    POSTGRES_SERVER: str = "localhost:5433"
     POSTGRES_USER: str = "postgres"
     POSTGRES_PASSWORD: str = "password"
     POSTGRES_DB: str = "transitops"
